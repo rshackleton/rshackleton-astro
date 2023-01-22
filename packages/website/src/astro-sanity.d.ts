5114b2fabd77942710585251c014f8528e6e490b
Richard Shackleton <shackleberry112@gmail.com>
2023-01-22 23:18:08 +0000

    add custom block components

--- packages/website/src/astro-sanity.d.ts
+++ packages/website/src/astro-sanity.d.ts
@@ -1,5 +1,8 @@
 declare module 'astro-sanity' {
   import type { SanityClient } from '@sanity/client';
+  import type { ImageUrlBuilder } from '@sanity/image-url/lib/types/builder';
+  import type { SanityClientLike } from '@sanity/image-url/lib/types/types';
+  const createImageBuilder: (client: SanityClientLike) => ImageUrlBuilder;
   const useSanityClient: () => SanityClient;
-  export { useSanityClient };
+  export { createImageBuilder, useSanityClient };
 }
