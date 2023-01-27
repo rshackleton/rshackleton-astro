8bc202e48002c61bb7cc666d8db7d44e7d16ad09
Richard Shackleton <shackleberry112@gmail.com>
2023-01-27 21:24:41 +0000

    remove unused content types

--- packages/studio/schemas/index.ts
+++ packages/studio/schemas/index.ts
@@ -1,9 +1,4 @@
 import { SchemaTypeDefinition } from 'sanity';
-import articleSummary from './blocks/articleSummary';
-import code from './blocks/code';
-import tweet from './blocks/tweet';
-import article from './documents/article';
-import contentPage from './documents/contentPage';
 import home from './documents/home';
 import master from './documents/master';
 import cta from './objects/cta';
@@ -14,14 +9,7 @@ import experience from './sections/experience';
 import hero from './sections/hero';
 
 export const schemaTypes: SchemaTypeDefinition[] = [
-  // Blocks
-  articleSummary,
-  code,
-  tweet,
-
   // Documents
-  article,
-  contentPage,
   home,
   master,
 
