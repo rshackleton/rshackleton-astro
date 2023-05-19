1d226908a86abb4a9569db5a04300bd089f8a414
Richard Shackleton <shackleberry112@gmail.com>
2023-05-19 18:44:31 +0100

    migrate to pnpm

--- packages/website/src/sanity/master.ts
+++ packages/website/src/sanity/master.ts
@@ -1,4 +1,4 @@
-import { useSanityClient } from 'astro-sanity';
+import { createClient } from '@sanity/client';
 import groq from 'groq';
 
 export type MasterModel = {
@@ -8,7 +8,12 @@ export type MasterModel = {
 };
 
 export async function getMaster() {
-  const client = useSanityClient();
+  const client = createClient({
+    apiVersion: new Date().toISOString().substring(0, 10),
+    dataset: import.meta.env.SANITY_STUDIO_DATASET,
+    projectId: import.meta.env.SANITY_STUDIO_PROJECT_ID,
+    useCdn: true,
+  });
 
   const data = await client.fetch(
     groq`
